6a5bd80fa203169ada4d9c28677b7672e05b431b
unknown <001279653@S348B1.labs.neit.edu>
2014-08-18 16:36:29 -0400

    sample_Lcd

---
+++ sketch_aug18a/sketch_aug18a.ino
@@ -0,0 +1,23 @@
+int i = 0;
+
+void initLcd(void);
+
+void setup ()
+{
+  Serial3.begin(9600);
+  Serial.begin(9600);
+  uint8_t init[] = {0xFE,0x58,0xfe,0x47,8,3};
+  
+  Serial3.write(init,6);
+  Serial3.write("test");
+
+  return; 
+}
+
+void loop ()
+
+{
+  
+  
+}
+
